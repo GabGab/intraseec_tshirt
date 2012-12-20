b6a7c2fb9feb7580e08c5c82d80171067f7fb1b7
vgabou <vincent.gabou@sociabliz.com>
2012-12-20 01:38:41 +0100

    First commit after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntraseecTshirt::Application.config.secret_token = '3355aa994752fc7e7ee54c2c260359266c95eafa36a1795fce63d5fd8651a2acd4f6b13edf51e549569864de6b7ec64b3234a05ca1a2c4bc016077cfab64d653'
