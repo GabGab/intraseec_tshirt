7ac0ef8c157bda3f11718bb36204df94ed7edd2c
vgabou <vincent.gabou@sociabliz.com>
2012-12-21 02:23:23 +0100

    Helpers and custom validations for user implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  def error_messages_for(record)
+    messages = []
+    record.errors.messages.each do |attribute, errors|
+      if attribute.to_s =~ /\./
+        names = attribute.to_s.split('.')
+        class_name = names.first.singularize
+        attribute = names.last
+      else
+        class_name = record.class.to_s.parameterize
+        attribute = attribute
+      end
+      pre_message = record.respond_to?(attribute.to_sym) ? 'Le champ ' : ''
+      message = pre_message + t("activerecord.attributes.#{class_name}.#{attribute}") + ' '
+      message += errors.first
+      messages << message
+    end
+
+    content_tag(:ul, :class => 'error_messages') do
+      messages.each do |message|
+        concat content_tag(:li, message, :class => 'error_message')
+      end
+    end if messages.present?
+  end
+
+  def facebook_like_button(url, options={})
+    content_tag(:div, nil, {
+      :class => 'fb-like',
+      'data-href' => url,
+      'data-send' => 'false',
+      'data-layout' => 'button_count',
+      'data-width' => options[:width] || '105',
+      'data-show-faces' => 'false'
+    })
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :expire, :first_name, :last_name, :token, :uid
 
+  new_user = Proc.new { |user| user.is_from_user_form }
+
+  attr_accessor :is_from_user_form
+  attr_accessible :is_from_user_form
+
   validates_presence_of :uid
+  validates_presence_of :email, :first_name, :last_name, :if => new_user
 end
