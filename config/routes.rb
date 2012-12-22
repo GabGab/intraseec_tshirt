b7813f001be44c973f66d888385ee47713e100c7
vgabou <vincent.gabou@sociabliz.com>
2012-12-22 02:19:46 +0100

    [Routes] New custom route created for Facebook app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ IntraseecTshirt::Application.routes.draw do
   match '/tab' => "sessions#create"
   root :to => 'users#show'
 
+  get 'sessions/create' => 'sessions#create', :as => 'create_session'
+
   resources :sessions, :only => ['new', 'create']
   resources :users, :only => ['edit', 'update', 'show']
 end
